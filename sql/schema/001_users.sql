d89ab6e946842d727c0c8e8eb0cb525b83b3488a
Emad Battal <emadbattal.work@gmail.com>
2023-11-26 13:54:09 +0300

    added docker + fixed sql generation

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE users (
-    id int NOT NULL,
+    id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    api_key VARCHAR(64) NOT NULL UNIQUE
 );
 
 -- +goose Down
