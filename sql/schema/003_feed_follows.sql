727af466d71111891499dd5ecb036b84e150c444
Emad Battal <emadbattal.work@gmail.com>
2023-12-01 16:12:25 +0300

    enchanced comments

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -5,7 +5,7 @@ CREATE TABLE feed_follows (
     updated_at TIMESTAMP NOT NULL,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
-    -- this will make sure that we can never have two instaneces of the same user-fade follow
+    -- this will make sure that we can never have two instaneces of the same user-feed follow
     -- user can follow a certain instance of a feed once
     UNIQUE(user_id, feed_id)
 );
